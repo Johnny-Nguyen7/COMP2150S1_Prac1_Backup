fa57df575ff07b6b81970e9236a02bb076a16f43
Johnny <johnny.nguyen6@students.mq.edu.au>
2021-07-28 19:27:00 +1000

    Update Mole.cs

--- COMP2160 Prac Week 1/Assets/Scripts/Mole.cs
+++ COMP2160 Prac Week 1/Assets/Scripts/Mole.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class Mole : MonoBehaviour
 {
+    public Color colour;
     // Start is called before the first frame update
+    private SpriteRenderer spriteRenderer;
     void Start()
     {
-        
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>(); 
+        sprite.color = colour;
     }
 
     // Update is called once per frame
